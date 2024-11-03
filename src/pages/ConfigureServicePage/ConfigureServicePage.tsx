f5b8e2860bfdd6bfd01b2714642a1265466240d3
Antonio <antonio.diminic@gmail.com>
2024-11-03 11:40:35 +0100

    removed unused code

--- src/pages/ConfigureServicePage/ConfigureServicePage.tsx
+++ src/pages/ConfigureServicePage/ConfigureServicePage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const ConfigureServicePage: React.FC = () => {
-  // Component state (optional, using React hooks)
-  // const [state, setState] = React.useState(initialState);
 
 
   return (
