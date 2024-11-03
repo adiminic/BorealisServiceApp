2d6eedbf8109f394927ff8be06096e685a33a7aa
Antonio <antonio.diminic@gmail.com>
2024-11-03 11:16:23 +0100

    added home page and navbar css

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+      <nav className="navbar">
+        <h2 className="nav-text-bold">Konfigurator servisa</h2>
+        <p className="nav-text-small">Izračunajte trošak servisa</p>
+      </nav>
+  );
+};
+
+export default NavBar;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
+import NavBar from "../../components/NavBar";
+import toolsIcon from "../../assets/tools-icon.svg";
+import "./HomePage.css";
 
 const HomePage: React.FC = () => {
-  // Component state (optional, using React hooks)
-  // const [state, setState] = React.useState(initialState);
-
   return (
     <>
+      <NavBar />
+      <div className="main-container">
+        <div className="home-container">
+          <img src={toolsIcon} alt="tools icon" />
+          <h1 className="text-xl font-bold">Konfigurator servisa</h1>
+          <p className="text-m">
+            Pošaljite upit za servis svog vozila pomoću našeg konfiguratora i
+            naš stučan tim će vam se javiti u najkraćem mogućem roku.
+          </p>
+          <button className="primary-button">Pokreni konfigurator</button>
+        </div>
+      </div>
     </>
   );
 };
-
 
 export default HomePage;
