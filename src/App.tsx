2cb861fa2e3ba9aa713e84bfeb2da96390f5b3f6
Antonio <antonio.diminic@gmail.com>
2024-11-04 19:56:41 +0100

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import RoutesPage from './pages/Routes/RoutesPage'
-import ServiceContextProvider from './context/ServiceContext'
+import { ServiceContextProvider } from './context/ServiceContext'
 
 function App() {
 
