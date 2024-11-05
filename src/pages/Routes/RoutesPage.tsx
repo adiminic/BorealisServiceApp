96003480fb1f067f2c8c4f0ab707e274a5857c4d
Antonio <antonio.diminic@gmail.com>
2024-11-05 21:00:33 +0100

    updated routes

--- src/pages/Routes/RoutesPage.tsx
+++ src/pages/Routes/RoutesPage.tsx
@@ -1,18 +1,27 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import HomePage from "../HomePage/HomePage"
-import ConfigureServicePage from "../ConfigureServicePage/ConfigureServicePage"
-import { StepRoute } from "../../util/constants"
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "../HomePage/HomePage";
+import ConfigureServicePage from "../ConfigureServicePage/ConfigureServicePage";
+import { StepRoute } from "../../util/constants";
+import ConfirmServicePage from "../ConfirmServicePage/ConfirmServicePage";
+import QuoteSentPage from "../QuoteSentPage/QuoteSentPage";
 
 const RoutesPage = () => {
-    return(
+  return (
     <>
-    <Routes>
+      <Routes>
         <Route path={StepRoute.home} element={<HomePage />} />
-        <Route path={StepRoute.configureService} element={<ConfigureServicePage />} />
+        <Route
+          path={StepRoute.configureService}
+          element={<ConfigureServicePage />}
+        />
+        <Route
+          path={StepRoute.confirmService}
+          element={<ConfirmServicePage />}
+        />
+        <Route path={StepRoute.quoteSent} element={<QuoteSentPage />} />
         <Route path="*" element={<Navigate to={StepRoute.home} replace />} />
-    </Routes>
+      </Routes>
     </>
-    )
-}
+  );
+};
 export default RoutesPage;
