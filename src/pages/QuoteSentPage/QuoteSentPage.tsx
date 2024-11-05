9fecccf6a5e171059d35d494dd5e4100aac5ff4f
Antonio <antonio.diminic@gmail.com>
2024-11-05 21:07:05 +0100

    added quote page

---
+++ src/pages/QuoteSentPage/QuoteSentPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import successIcon from "../../assets/success-icon.svg";
+import "./QuoteSentPage.css";
+
+const QuoteSentPage: React.FC = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="main-container">
+        <div className="home-container">
+          <img src={successIcon} alt="success icon" />
+          <h1 className="text-xl font-bold">
+            Vaša prijava je uspješno poslana
+          </h1>
+          <p className="text-m">
+            Vaša prijava je uspješno poslana i zaprimljena. Kontaktirat ćemo vas
+            u najkraćem mogućem roku.
+          </p>
+          <p className="text-m">Hvala vam!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default QuoteSentPage;
